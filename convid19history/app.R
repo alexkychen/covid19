a51c504c3b1bd75bcfe4217a44374467d5e15a30
alexkychen <alexkychen@gmail.com>
2020-03-13 16:08:18 -0400

    update

--- convid19history/app.R
+++ convid19history/app.R
@@ -18,6 +18,10 @@ data_confirmed <- read.csv(URL_Confirmed)
 data_deaths <- read.csv(URL_Deaths)
 data_recovered <- read.csv(URL_Recovered)
 
+#Get date
+date <- as.Date("2020-01-22") + 0:(ncol(data_confirmed)-5)
+
+#Get country
 countryList <- levels(data_confirmed$Country.Region)
 countryList <- c("None", countryList)
 
@@ -32,16 +36,17 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             dateRangeInput("daterange", "Date range", start="2020-01-22", format="mm-dd"),
+            helpText("Earliest report date is January 22nd, 2020"),
+            helpText(""),
             selectInput("country1", "Select a country", choices=countryList, selected="Taiwan*"),
             selectInput("country2", "Select a country", choices=countryList, selected="China"),
             selectInput("country3", "Select a country", choices=countryList, selected="None"),
             selectInput("country4", "Select a country", choices=countryList, selected="None"),
-            actionButton("plot","Make plots")
+            actionButton("go",label="Make plots")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           #textOutput("date_range"),
            plotOutput("confirmed_accu"),
            plotOutput("deaths_accu"),
            plotOutput("mortality")
@@ -51,14 +56,71 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    #output$date_range <- renderText({paste0("You select date range", input$daterange[2])})
 
-    output$confirmed_accu <- renderPlot({
-        #ggplot(data=df, aes(x=,y=))+
-        #geom_point()+
-        #xlab("Day")+ylab("Accumulated confirmed CONVID-19 cases")+
-        #theme_bw()
-    })
+  res <- eventReactive(input$go, {
+    
+    #get selected country name
+    countries <- c(input$country1,input$country2,input$country3,input$country4)
+    
+    #create empty data frame
+    df <- data.frame(matrix(ncol=0, nrow=0), stringsAsFactors=T)
+    
+    #compile data for confirmed cases
+    for(i in countries){
+      if(i != "None"){
+        #get confirmed cases
+        one_country <- subset(data_confirmed, Country.Region==i, select=5:ncol(data_confirmed))
+        case_sum <- colSums(one_country)
+        #get death cases
+        one_country_death <- subset(data_deaths, Country.Region==i, select=5:ncol(data_deaths))
+        death_sum <- colSums(one_country_death)
+        
+        if(i == "Taiwan*"){
+          one_df <- data.frame(date=date, cases=case_sum, death=death_sum, country=rep("Taiwan", length(case_sum)), stringsAsFactors=T)
+        }else{
+          one_df <- data.frame(date=date, cases=case_sum, death=death_sum, country=rep(i, length(case_sum)), stringsAsFactors=T)
+        }
+        df <- rbind(df, one_df)
+      }
+    }
+    
+    #make confirmed case plot
+    plot_confirmed <- ggplot(data=df, aes(x=date, y=cases, color=country))+
+      geom_point(shape=19)+
+      scale_colour_hue(l=50)+
+      scale_x_date(date_labels = "%b-%d", date_breaks = "1 week", limits = c(input$daterange[1], input$daterange[2]))+
+      ggtitle("Accumulated Cases of Confirmed CONVID-19")+
+      xlab("Date")+ylab("Number of cases")+
+      theme_bw()+
+      theme(axis.title=element_text(size=16),axis.text=element_text(size=12),
+            plot.title=element_text(size=18,face="bold"),
+            legend.title=element_text(size=16),legend.text=element_text(size=16))
+    
+    #make death case plot
+    plot_deaths <- ggplot(data=df, aes(x=date, y=death, color=country))+
+      geom_point(shape=19)+
+      scale_colour_hue(l=50)+
+      scale_x_date(date_labels = "%b-%d", date_breaks = "1 week", limits = c(input$daterange[1], input$daterange[2]))+
+      ggtitle("Accumulated Deaths")+
+      xlab("Date")+ylab("Number of deaths")+
+      theme_bw()+
+      theme(axis.title=element_text(size=16),axis.text=element_text(size=12),
+            plot.title=element_text(size=18,face="bold"),
+            legend.title=element_text(size=16),legend.text=element_text(size=16))
+    
+    #output results
+    list(plot_confirmed=plot_confirmed, plot_deaths=plot_deaths)
+    
+  })#for res <- eventReactive
+  
+  #render plot
+  output$confirmed_accu <- renderPlot({
+    res()$plot_confirmed
+  })
+  
+  output$deaths_accu <- renderPlot({
+    res()$plot_deaths
+  })
 }
 
 # Run the application 
